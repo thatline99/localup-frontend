43659d4f4aeeb1c3abf4d6cfdd127e7d80234295
49EHyeon42 <49ehyeon42@gmail.com>
2025-06-02 18:25:57 +0900

    chore: 설정 확인 1

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {};
-
-// 개발 모드에서만 Cloudflare Edge 환경으로 개발 서버 테스트
-if (process.env.NODE_ENV === "development") {
-  (async () => {
-    await setupDevPlatform();
-  })();
-}
 
 export default nextConfig;
