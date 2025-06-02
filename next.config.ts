242e05379e86cb48be08b8ad8161e1d170d9ffac
49EHyeon42 <49ehyeon42@gmail.com>
2025-06-02 18:20:16 +0900

    chore: 다시 작성

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-};
+const nextConfig: NextConfig = {};
 
 // 개발 모드에서만 Cloudflare Edge 환경으로 개발 서버 테스트
 if (process.env.NODE_ENV === "development") {
