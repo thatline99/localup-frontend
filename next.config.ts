f2a1110d8ba78460f4affb6f68854b5927f02967
49EHyeon42 <49ehyeon42@gmail.com>
2025-06-02 15:12:16 +0900

    chore: Next.js, Cloudflare Pages 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
   output: "standalone",
 };
+
+// 개발 모드에서만 Cloudflare Edge 환경으로 개발 서버 테스트
+if (process.env.NODE_ENV === "development") {
+  (async () => {
+    await setupDevPlatform();
+  })();
+}
 
 export default nextConfig;
