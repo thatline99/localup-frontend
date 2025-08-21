d8130c7f6040d1f7ce52ccdab6e40bc41972931e
49EHyeon42 <49ehyeon42@gmail.com>
2025-08-21 19:02:26 +0900

    fix: Next.js 이미지 최적화, HTTPS도 허용할 수 있도록 수정

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/cms/resource/**",
       },
+      {
+        protocol: "https",
+        hostname: "tong.visitkorea.or.kr",
+        port: "",
+        pathname: "/cms/resource/**",
+      },
     ],
   },
 };
